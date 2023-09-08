f4c8a41b9a0403ef7e39769174121d5eb5235526
Alex Kolesnyk <alexander.v.anderson@gmail.com>
2023-09-07 21:13:30 -0400

    Safari first try Sep 2023

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -15,27 +16,35 @@ public class BaseTest {
     protected Stories stories;
 
 
-    @BeforeSuite
-    public void beforeSuite() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\alexander.anderson\\IdeaProjects\\InstaProject\\chromedriver.exe");
-    }
+//    @BeforeSuite
+//    public void beforeSuite() {
+//        System.setProperty("webdriver.chrome.driver", "/Users/oleksii_kolesnik/IdeaProjects/InstaProject/chromedriver");
+//    }
 
     @BeforeMethod
     public void startUp() {
 
-        // Set Chrome options to disable notifications
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-notifications");
+        // Initialize SafariDriver
+        driver = new SafariDriver();
 
-        // Pass the options when initializing ChromeDriver
-        driver = new ChromeDriver(options);
+
+
+//        // Set Chrome options to disable notifications
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-notifications");
+//
+//        // Pass the options when initializing ChromeDriver
+//        driver = new ChromeDriver(options);
+
+
+
         driver.manage().window().maximize();
 
         // I keep this data for later
         username = "Alex_kolesnyk_";
         password = "Family2022!";
 
-////         this data for now
+//         this data for now
 //        username = "odessa_mma_team_";
 //        password = "Mittswork2023!";
 
@@ -53,9 +62,38 @@ public class BaseTest {
         // Initialize the Stories page (only once)
         stories = new Stories(driver);
     }
-    @AfterMethod
-    public void tearDown() throws InterruptedException {
-        Thread.sleep(3000);
-        driver.quit();
-    }
+//    @AfterMethod
+//    public void tearDown() throws InterruptedException {
+//        Thread.sleep(3000);
+//        driver.quit();
+//    }
+
+
+//    private void disableSafariNotifications() {
+//        try {
+//            // Execute AppleScript to disable Safari notifications
+//            String appleScript = "tell application \"Safari\" to activate\n" +
+//                    "delay 1\n" +
+//                    "tell application \"System Events\"\n" +
+//                    "    tell process \"Safari\"\n" +
+//                    "        click menu item \"Safari\" of menu \"Safari\" of menu bar item \"Safari\" of menu bar 1\n" +
+//                    "        delay 1\n" +
+//                    "        click menu item \"Preferences…\" of menu \"Safari\" of menu bar item \"Safari\" of menu bar 1\n" +
+//                    "        delay 1\n" +
+//                    "        click button \"Notifications\" of tool bar 1 of window 1\n" +
+//                    "        delay 1\n" +
+//                    "        click radio button \"Deny\" of group 1 of group 1 of window 1\n" +
+//                    "        delay 1\n" +
+//                    "        click button \"Allow\" of window 1\n" +
+//                    "    end tell\n" +
+//                    "end tell";
+//            Runtime.getRuntime().exec(new String[]{"osascript", "-e", appleScript});
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
+
+
 }
